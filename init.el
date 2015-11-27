25a8dc44d7c59da9a1d7b8fcb8b2c290595684d0
Yu Yang Tian <yu.yang.tian@autodesk.com>
2015-11-27 16:58:32 -0500

    Initial working config

---
+++ init.el
@@ -0,0 +1,33 @@
+(unless (gnutls-available-p) (error "GNU TLS is not available (are you using Windows? You may have to manually install it)"))
+
+
+(desktop-save-mode 1)
+
+;; Remove scrollbars, menu bars, and toolbars
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(custom-enabled-themes (quote (wheatgrass))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(defvar root-dir (file-name-directory load-file-name)
+  "The root dir of the Emacs configuration.")
+(defvar package-dir (expand-file-name "pkg" root-dir)
+  "Package management.")
+
+(add-to-list 'load-path package-dir)
+(require 'install-packages)
---
+++ pkg/install-packages.el
@@ -0,0 +1,32 @@
+(require 'cl)
+(require 'package)
+
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(defvar required-packages
+  '(
+    magit
+    org
+    clojure-mode
+  ) "The list of packages that must be installed before launch.")
+
+;; method to check if all packages are installed
+(defun packages-installed-p ()
+  ; Note: look up how this works (common lisp macro)
+  (loop for p in required-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+;; if not all packages are installed, check one by one and install the missing ones.
+(unless (packages-installed-p)
+  ; check for new packages (package versions)
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ; install the missing packages
+  (dolist (p required-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(provide 'install-packages)
