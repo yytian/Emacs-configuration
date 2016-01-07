420cc51757d456a521d35c72ccf3cd3fba3c857d
Jim Tian <yytian@uwaterloo.ca>
2016-01-07 17:19:47 -0500

    Add Auctex, ensure use-package

--- pkg/install-packages.el
+++ pkg/install-packages.el
@@ -1,22 +1,50 @@
 (require 'package)
 
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 (eval-when-compile
   (require 'use-package))
 ;(require 'diminish)                ;; if you use :diminish
 (require 'bind-key)                ;; if you use any :bind variant
 
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-
-(setq use-package-always-ensure t)
+(setq use-package-always-ensure t) ;; A bit redundant but that's fine
 
 (use-package magit
-	     :ensure t)
+  :ensure t)
 
 (use-package org
-	     :ensure t)
+  :ensure t)
 
 (use-package clojure-mode
-	     :ensure t)
+  :defer t
+  :ensure t)
+
+(use-package paredit
+  :ensure t
+  :init
+  (add-hook 'clojure-mode-hook 'enable-paredit-mode)
+  (add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook 'enable-paredit-mode)
+  (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode))
+
+(use-package cider
+  :ensure t)
+
+(use-package auctex
+  :ensure t
+  :mode ("\\.tex\\'" . latex-mode)
+  :commands (latex-mode LaTeX-mode plain-tex-mode)
+  :init
+  (progn
+    (setq TeX-auto-save t
+	  TeX-parse-self t
+	  TeX-save-query nil
+	  TeX-PDF-mode t)))
 
 (provide 'install-packages)
